440d54f102d14a5284af4d724abb383cb3b32b8f
HiroForYou <csanchezs@uni.pe>
2022-07-12 14:49:52 -0500

    Updates

---
+++ precache-manifest.c8bff4ee8115872eadef77bb07352917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dde5218c523aeff78d3eda0ed8e10b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e529bd82e0253c6d9821",
+    "url": "/react-gh-pages/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "24202e8ec82283f5a984",
+    "url": "/react-gh-pages/static/js/2.cfce73c6.chunk.js"
+  },
+  {
+    "revision": "94db950e4c9c0176fe2653d1cd4d5944",
+    "url": "/react-gh-pages/static/js/2.cfce73c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e529bd82e0253c6d9821",
+    "url": "/react-gh-pages/static/js/main.73c93157.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-gh-pages/static/js/main.73c93157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077e60b225389c05c46d",
+    "url": "/react-gh-pages/static/js/runtime-main.8fca155e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-gh-pages/static/media/logo.ee7cd8ed.svg"
+  }
+]);
